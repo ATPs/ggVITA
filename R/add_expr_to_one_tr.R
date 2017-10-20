5e29482c903f3cab94d004d9cc80f32c2b3bc614
icyvodka <942731366@qq.com>
2017-10-20 19:55:57 +0800

    Update add_expr_to_one_tr.R

--- R/add_expr_to_one_tr.R
+++ R/add_expr_to_one_tr.R
@@ -30,7 +30,7 @@ add_expr_2_one_tr<-function(expr_file,
   
   epic_gene_expr_simple<-data.frame(epic_gene_expr)[,col_names] %>% data.table()
   
-  head(epic_gen_expr_simple)
+  head(epic_gene_expr_simple)
   
   #head(epic_gene_expr_simple)
   
